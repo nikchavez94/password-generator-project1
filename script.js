572587d035d153099f0bd6d99e1b572374846e08
Nikki Vargas <nikkivargas@Nikkis-iMac.local>
2023-09-11 21:58:29 -0500

    updated javascript

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-// Assignment code here
+var numbers = "1234567890"
+var symbols = "!@#%?*"
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowercase = uppercase.toLowerCase()
+var characterchoices = ""
+var endingPassword = ""
 
+function generatePassword (){
+  var passwordLength = prompt("How long would you like the password?")
+  var confirmNum = confirm("Do you want numbers in your password?")
+  var confirmSym = confirm("Do you want symbols in your password?")
+  var confirmUpper = confirm("Do you want uppercase letters in your password?")
+  var confirmLow = confirm("Do you want lowercase letters in your password?")
+if (confirmNum === true) {
+  characterchoices += numbers 
+}
+if (confirmSym === true) {
+  characterchoices += symbols
+}
+if (confirmUpper === true) {
+  characterchoices += uppercase
+}
+if (confirmLow === true) {
+  characterchoices += lowercase
+}
+
+if (!confirmNum && !confirmLow && !confirmSym && !confirmUpper) {
+  alert("You must make a choice!")
+}
+
+for (var i =0;i<passwordLength; i++){
+var randomizedCharacter = Math.floor(Math.random()*characterchoices.length)
+ endingPassword += characterchoices.charAt(randomizedCharacter)
+}
+return endingPassword
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
